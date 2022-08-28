d191e05931a7bd1da6e2b24619bea3a8beefe5fb
Commander-Cat101 <102434202+Commander-Cat101@users.noreply.github.com>
2022-08-28 17:29:50 +1200

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.Simulation;
+using Assets.Scripts.Simulation.Towers;
+using Assets.Scripts.Unity.UI_New.InGame;
+using BTD_Mod_Helper;
+using BTD_Mod_Helper.Extensions;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(TemplateMod.Main), "Cheerleader", "1.0.0", "Commander__Cat")]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+namespace TemplateMod
+{
+    public class Main : BloonsTD6Mod
+    {
+        
+    }
+}
